ac794017de86f25759988fec6c7216c932eb9d0a
bdipin <91279927+bdipin@users.noreply.github.com>
2021-10-09 10:06:21 +0530

    Add files via upload

---
+++ Practical-06/Task-06/Task6.cpp
@@ -0,0 +1,61 @@
+  
+/*
+Practical Number: 6
+Task 6
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string a = "Hello";
+	cout<<a<<"\n"<<&a<<endl;
+	a[0]='J';
+	cout<<a<<endl;
+	cout<<&a<<endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
